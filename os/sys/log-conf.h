11221bfb87111ecc70742697a668988e4ddba72f
NuzhatZaidi <53250214+NuzhatZaidi@users.noreply.github.com>
2020-02-17 14:45:15 +0500

    Update log-conf.h

--- os/sys/log-conf.h
+++ os/sys/log-conf.h
@@ -103,7 +103,9 @@
 /******************************************************************************/
 
 #ifndef LOG_CONF_LEVEL_RPL
-#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_NONE /* Only for rpl-lite */
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_ERR /* Only for rpl-lite */
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_WARN /* Only for rpl-lite */
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_INFO /* Only for rpl-lite */
 #endif /* LOG_CONF_LEVEL_RPL */
 
 #ifndef LOG_CONF_LEVEL_TCPIP
