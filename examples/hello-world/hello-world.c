1cf64eaa1930fb0e134431a87860e47c0dd6f03f
NuzhatZaidi <53250214+NuzhatZaidi@users.noreply.github.com>
2020-03-11 14:30:37 +0500

    Update hello-world.c

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -60,7 +60,7 @@ PROCESS_THREAD(hello_world_process, ev, data)
 
   while(1) {
     printf("Hello Nuzhat\n");
-    printf("Transmissio power of this node is %d \n", CC2538_RF_TX_POWER_RECOMMENDED);
+    printf("Transmissio power: %d \n", CC2538_RF_TX_POWER_RECOMMENDED);
 
     /* Wait for the periodic timer to expire and then restart the timer. */
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
