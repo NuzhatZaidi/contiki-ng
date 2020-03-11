1ca4fde5df6fc9dc8fb16c469445591abff627e0
NuzhatZaidi <53250214+NuzhatZaidi@users.noreply.github.com>
2020-03-11 12:56:05 +0500

    Update hello-world.c

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -38,7 +38,10 @@
  */
 
 #include "contiki.h"
-
+#include "dev/cc2538-rf.h"
+#include "dev/radio.h"
+#include "dev/rfcore.h"
+#include "reg.h"
 #include <stdio.h> /* For printf() */
 /*---------------------------------------------------------------------------*/
 PROCESS(hello_world_process, "Hello world process");
@@ -52,9 +55,12 @@ PROCESS_THREAD(hello_world_process, ev, data)
 
   /* Setup a periodic timer that expires after 10 seconds. */
   etimer_set(&timer, CLOCK_SECOND * 10);
+ 
+  
 
   while(1) {
     printf("Hello Nuzhat\n");
+    printf("Transmissio power of this node is \n %X", CC2538_RF_TX_POWER_RECOMMENDED);
 
     /* Wait for the periodic timer to expire and then restart the timer. */
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
