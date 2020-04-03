9c0f7a0253ff6398fd8d94070dce5e198fea0882
NuzhatZaidi <53250214+NuzhatZaidi@users.noreply.github.com>
2020-04-03 14:44:40 +0500

    Update hello-world.c

--- examples/hello-world/hello-world.c
+++ examples/hello-world/hello-world.c
@@ -41,8 +41,8 @@
 #include "dev/cc2538-rf.h"
 #include "sys/node-id.h"
 
-#include "dev/cc2420/cc2420_const.h"
-#include "dev/cc2420/cc2420.h"
+//#include "dev/cc2420/cc2420_const.h"
+//#include "dev/cc2420/cc2420.h"
 #include "net/netstack.h"
 
 #include "dev/radio.h"
@@ -65,12 +65,12 @@ PROCESS_THREAD(hello_world_process, ev, data)
     //int txVal;
   while(1) {
     printf("Hello Nuzhat\n");
-    //char rd;
+    char rd;
     //cc2420_set_txpower(15);
     //printf("%d", txVal = cc2420_get_txpower());
     
-    //rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, 19);
-    //printf("Transmissio power of %c: \n", rd);
+    rd = NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, 19);
+    printf("Transmissio power of %c: \n", rd);
     //printf("Transmissio power of %d : %x \n", node_id, CC2538_RF_TX_POWER_RECOMMENDED);
     /* Wait for the periodic timer to expire and then restart the timer. */
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
